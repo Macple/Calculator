b09c0c9bc3fcf01b4c36df23c56357b835dfc90b
Maciej Plewko <maciej.plewko@gmail.com>
2023-04-01 00:44:30 +0200

    Inital iOS project

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Maciej Plewko on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
